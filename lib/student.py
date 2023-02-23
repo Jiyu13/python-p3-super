e1687db170ba22831c4202f07689366beacfee64
Ziru Zou <jiyucoding@yahoo.com>
2023-02-23 10:44:07 -0500

    define __init__() that initialises name and grade attribute

--- lib/student.py
+++ lib/student.py
@@ -1,7 +1,30 @@
-from use import User
+from user import User
 
 
 class Student(User):
+
+    def __init__(self, name, grade):
+        print("Student.log_in() called.")
+        self.name = name
+        self.grade = grade
+
+
     def log_in(self):
+        print("Stuednt.log_in() called.")
+
+        #  super() calls one the log_in() as defined in the superclass
         super().log_in()
+
+        # supercharge with new attribute that only for Student class
         self.in_class = True
+
+
+oneil = Student()
+oneil.log_in()
+print(oneil.in_class)
+
+# Stuednt.log_in() called.
+# User.log_in() called.
+# True
+
+
